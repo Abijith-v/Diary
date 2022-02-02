ee2d14265922e07671dcf4c627e70b62b21a4570
Abijith-v <ajuvinod333@gmail.com>
2022-02-02 06:18:08 +0530

    Added journal page and color selector

--- app/src/main/java/com/example/diary/MainActivity.java
+++ app/src/main/java/com/example/diary/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -28,6 +29,8 @@ import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 import org.jetbrains.annotations.NotNull;
 
+import dev.shreyaspatil.MaterialDialog.BottomSheetMaterialDialog;
+import dev.shreyaspatil.MaterialDialog.MaterialDialog;
 import io.ak1.BubbleTabBar;
 import io.ak1.OnBubbleClickListener;
 
@@ -59,56 +62,84 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
-                View mview = getLayoutInflater().inflate(R.layout.add_diary_dialog,null);
+//                final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+//                View mview = getLayoutInflater().inflate(R.layout.add_diary_dialog,null);
+//
+//                RelativeLayout rootLayout = mview.findViewById(R.id.dialogLayout);
+//                LinearLayout expandableCalLayout = mview.findViewById(R.id.dropDownSectionDialog);
+//                MaterialButton okButton = mview.findViewById(R.id.okButtonAddNewDialog);
+//                MaterialButton moreButton = mview.findViewById(R.id.moreButtonAddNewDialog);
+//                CalendarView calendar = mview.findViewById(R.id.calViewForDialog);
+//
+//
+//                Transition transition = new Slide(Gravity.TOP);
+//                transition.setDuration(700);
+//                transition.addTarget(calendar);
+//
+//                alert.setView(mview);
+//                final AlertDialog alertDialog = alert.create();
+//                alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//                alertDialog.setCanceledOnTouchOutside(true);
+//
+//                final boolean[] expanded = {false};
+//
+//                moreButton.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//
+//                        if(expanded[0] == false) {
+//
+//                            TransitionManager.beginDelayedTransition(rootLayout, transition);
+//
+//                            calendar.setVisibility(View.VISIBLE);
+//                            expanded[0] = true;
+//                        }
+//                        else {
+//                            calendar.setVisibility(View.GONE);
+//                            expanded[0] = false;
+//                        }
+//                    }
+//                });
+//
+//                okButton.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//
+//                        startActivity(new Intent(MainActivity.this, makeJournal.class));
+//                        alertDialog.dismiss();
+//                    }
+//                });
+//
+//
+//                alertDialog.show();
 
-                RelativeLayout rootLayout = mview.findViewById(R.id.dialogLayout);
-                LinearLayout expandableCalLayout = mview.findViewById(R.id.dropDownSectionDialog);
-                MaterialButton okButton = mview.findViewById(R.id.okButtonAddNewDialog);
-                MaterialButton moreButton = mview.findViewById(R.id.moreButtonAddNewDialog);
-                CalendarView calendar = mview.findViewById(R.id.calViewForDialog);
+
+                BottomSheetMaterialDialog mDialog = new BottomSheetMaterialDialog.Builder(MainActivity.this)
+                        .setTitle("Add a journal?")
+                        .setMessage("Do you want to write a new journal?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", R.drawable.tick_icon, new MaterialDialog.OnClickListener() {
+                            @Override
+                            public void onClick(dev.shreyaspatil.MaterialDialog.interfaces.DialogInterface dialogInterface, int which) {
+
+                                startActivity(new Intent(MainActivity.this, makeJournal.class));
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .setNegativeButton("Cancel", R.drawable.cancel_icon, new MaterialDialog.OnClickListener() {
+                            @Override
+                            public void onClick(dev.shreyaspatil.MaterialDialog.interfaces.DialogInterface dialogInterface, int which) {
+
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .setAnimation(R.raw.walking)
+                        .build();
+
+                // Show Dialog
+                mDialog.show();
 
 
-                Transition transition = new Slide(Gravity.TOP);
-                transition.setDuration(700);
-                transition.addTarget(calendar);
-
-                alert.setView(mview);
-                final AlertDialog alertDialog = alert.create();
-                alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                alertDialog.setCanceledOnTouchOutside(true);
-
-                final boolean[] expanded = {false};
-
-                moreButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                        if(expanded[0] == false) {
-
-                            TransitionManager.beginDelayedTransition(rootLayout, transition);
-
-                            calendar.setVisibility(View.VISIBLE);
-                            expanded[0] = true;
-                        }
-                        else {
-                            calendar.setVisibility(View.GONE);
-                            expanded[0] = false;
-                        }
-                    }
-                });
-
-                okButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                        startActivity(new Intent(MainActivity.this, makeJournal.class));
-                        alertDialog.dismiss();
-                    }
-                });
-
-
-                alertDialog.show();
             }
         });
 
@@ -121,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(int i) {
 
                 Fragment frag = null;
-                switch(i) {
+                switch (i) {
                     case R.id.bottom_nav_home:
 
                         frag = new homeFrag();
--- app/src/main/java/com/example/diary/makeJournal.java
+++ app/src/main/java/com/example/diary/makeJournal.java
@@ -2,13 +2,64 @@ package com.example.diary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class makeJournal extends AppCompatActivity {
+import com.google.android.material.textview.MaterialTextView;
+import com.jaredrummler.android.colorpicker.ColorPickerDialog;
+import com.jaredrummler.android.colorpicker.ColorPickerDialogListener;
+import com.jaredrummler.android.colorpicker.ColorPickerView;
+
+import static com.jaredrummler.android.colorpicker.ColorPickerDialog.TYPE_CUSTOM;
+
+public class makeJournal extends AppCompatActivity implements ColorPickerDialogListener {
+
+    MaterialTextView dateSelector;
+    ImageView colorSelector;
+
+    public final static int DIALOG_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_journal);
+
+        dateSelector = findViewById(R.id.todaysDateInTopBar);
+        colorSelector = findViewById(R.id.selectColor);
+
+        dateSelector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        colorSelector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ColorPickerDialog.newBuilder()
+                        .setDialogType(ColorPickerDialog.TYPE_PRESETS)
+                        .setAllowPresets(true)
+                        .setDialogId(DIALOG_ID)
+                        .setColor(Color.BLUE)
+                        .setShowAlphaSlider(true)
+                        .show(makeJournal.this);
+            }
+        });
+    }
+
+
+    @Override public void onColorSelected(int dialogId, int color) {
+
+        if(dialogId == DIALOG_ID)
+            Toast.makeText(this, Integer.toHexString(color), Toast.LENGTH_SHORT).show();
+    }
+    @Override public void onDialogDismissed(int dialogId) {
+
+//        Toast.makeText(this, "dismissed BC", Toast.LENGTH_SHORT).show();
     }
 }
